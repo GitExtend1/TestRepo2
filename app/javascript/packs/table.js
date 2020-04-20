2802d68afd4645b70452e0749d3cf7840c481f19
Raj Saha <krajsaha@gmail.com>
2020-04-20 19:02:05 +0530

    Add files via upload

---
+++ app/javascript/packs/table.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const TableItem=(props)=>{
+
+
+return(
+
+
+<>
+
+    <p>{props.name}</p>
+    <p>{props.age}</p>
+    <p>{props.country}</p>
+  
+</>
+
+
+
+)
+
+
+}
+
+
+
+
+
+
+export default TableItem
